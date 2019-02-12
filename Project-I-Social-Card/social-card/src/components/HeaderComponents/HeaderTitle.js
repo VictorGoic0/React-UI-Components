19fd7681f6af4427673e492e3f5d81fa58c021a5
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-02-11 19:45:19 -0500

    header components completed

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div className="header-title">
+      <h4>Lambda School</h4>
+      <h5>@LambdaSchool</h5>
+      <h6>26 Jan</h6>
+    </div>
+  );
+}
+
+export default HeaderTitle;
